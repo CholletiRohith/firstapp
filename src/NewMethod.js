9f0542cf11c046d72b46bc21899ee5dc1ab0761f
Rohith <rohithcholleti97@gmail.com>
2020-11-12 19:08:20 +0530

    crashcourse

---
+++ src/NewMethod.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import "./App.css";
+
+
+
+function NewMethod()
+{
+  return(
+    <div className="App">
+      <h1 className="sukku ">Sukanya</h1>
+    </div>
+  );
+}
+export default NewMethod;
